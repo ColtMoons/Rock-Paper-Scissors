22d486241bb31ca8165b2e30db58f61ae0dea26f
ColtMoons <coltmoonsac@gmail.com>
2022-07-29 16:18:20 -0500

    Create a Random Computer Choice function

---
+++ rock-paper-scissors.js
@@ -0,0 +1,14 @@
+function getComputerChoice(){
+    let rand = Math.floor(Math.random()*3);
+    switch(rand){
+        case 0:
+            return "Rock"
+            break;
+        case 1:
+            return "Paper"
+            break;
+        case 2:
+            return "Scissors"
+    }
+}
+
